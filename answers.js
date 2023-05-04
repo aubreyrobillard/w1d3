2c249170740e95eefbec0af1864c41029d39b976
Aubrey Robillard <aubreyrobillard@Aubreys-MacBook-Pro.local>
2023-05-03 22:21:27 -0500

    Get Even Answered

--- answers.js
+++ answers.js
@@ -10,7 +10,10 @@
 for (i = 1; i <= 20; i++)
     console.log(i)
 
-
+//Get Even:
+for (i = 0; i <= 200; i++)
+    if(i % 2 === 0)
+        console.log(i)
 
 ////////////////////////////////
 // Fizz Buzz
