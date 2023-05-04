4a3d44df455b863b57b8e5bb71f7a6f5a4bfb878
Aubrey Robillard <aubreyrobillard@Aubreys-MacBook-Pro.local>
2023-05-03 23:14:31 -0500

    Wild Wild Life answered

--- answers.js
+++ answers.js
@@ -20,32 +20,52 @@
 // Fizz Buzz
 ////////////////////////////////
 //1. Write a javascript application that logs all numbers from 1 - 100.
-for (i = 1; i <= 100; i++)
-    console.log(i)
+// for (i = 1; i <= 100; i++)
+//     console.log(i)
 
-    //2. If a number is divisible by 3 log "Fizz" instead of the number.
-for (i = 1; i <= 100; i++) 
-    if (i % 3 === 0)
-        console.log("Fizz") 
-            else console.log(i)
+//2. If a number is divisible by 3 log "Fizz" instead of the number.
+// for (i = 1; i <= 100; i++) 
+//     if (i % 3 === 0)
+//         console.log("Fizz") 
+//             else console.log(i)
 
 //3. If a number is divisible by 5 log "Buzz" instead of the number.
-for (i = 1; i <= 100; i++) 
-    if (i % 5 === 0)
-        console.log("Buzz") 
-            else console.log(i)
+// for (i = 1; i <= 100; i++) 
+//     if (i % 5 === 0)
+//         console.log("Buzz") 
+//             else console.log(i)
 
 //4. If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number.
-for (i = 1; i <= 100; i++) 
-    if ((i % 3 === 0) && (i % 5 === 0))
-        console.log("FizzBuzz") 
-    else console.log(i)
+// for (i = 1; i <= 100; i++) 
+//     if ((i % 3 === 0) && (i % 5 === 0))
+//         console.log("FizzBuzz") 
+//     else console.log(i)
 
 
 ////////////////////////////////
 // Wild Wild Life
 ////////////////////////////////
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
 
+// 1. Plantee just had her birthday; change Plantee's array to reflect her being a year older.
+plantee[2] = 5001
+    console.log(plantee)
+
+// 2. Change Wolfy's hometown from "Yukon Territory" to "Gotham City".
+wolfy[3] = ("Gotham City")
+    console.log(wolfy)
+
+// 3. Give D'Art a second hometown by adding "Hawkins"
+wolfy.push("Hawkins")
+    console.log(wolfy)
+
+//4. Porgee decides that Wolfy can't be named "Wolfy" anymore. Remove "Wolfy" from the wolfy array and replace it with "Gameboy".
+wolfy.shift(), wolfy.unshift("Gameboy")
+    console.log(wolfy)
 
 ////////////////////////////////
 // Yell at the Ninja Turtles
