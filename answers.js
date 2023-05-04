92847a5985bf0b96041ed11b776ec215cf7b4bde
Aubrey Robillard <aubreyrobillard@Aubreys-MacBook-Pro.local>
2023-05-04 00:30:50 -0500

    Yell at the Ninja Turtles answered

--- answers.js
+++ answers.js
@@ -45,32 +45,40 @@
 ////////////////////////////////
 // Wild Wild Life
 ////////////////////////////////
-const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
-const sharky = ["Sharky", "shark", 20, "Left Coast"]
-const plantee = ["Plantee", "plant",  5000 , "Mordor"]
-const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
-const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+// const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+// const sharky = ["Sharky", "shark", 20, "Left Coast"]
+// const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+// const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+// const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
 
 // 1. Plantee just had her birthday; change Plantee's array to reflect her being a year older.
-plantee[2] = 5001
-    console.log(plantee)
+// plantee[2] = 5001
+//     console.log(plantee)
 
 // 2. Change Wolfy's hometown from "Yukon Territory" to "Gotham City".
-wolfy[3] = ("Gotham City")
-    console.log(wolfy)
+// wolfy[3] = ("Gotham City")
+//     console.log(wolfy)
 
 // 3. Give D'Art a second hometown by adding "Hawkins"
-wolfy.push("Hawkins")
-    console.log(wolfy)
+// wolfy.push("Hawkins")
+//     console.log(wolfy)
 
 //4. Porgee decides that Wolfy can't be named "Wolfy" anymore. Remove "Wolfy" from the wolfy array and replace it with "Gameboy".
-wolfy.shift(), wolfy.unshift("Gameboy")
-    console.log(wolfy)
+// wolfy.shift(), wolfy.unshift("Gameboy")
+//     console.log(wolfy)
 
 ////////////////////////////////
 // Yell at the Ninja Turtles
 ////////////////////////////////
+// 1. Create an array with the members of the ninja turtles (Donatello, Leonardo, Raphael, Michaelangelo)
+// const ninjaTurtles = ["Donatello", "Leonardo", "Raphael", "Michaelangelo"]
+    // console.log(ninjaTurtles)
 
+// 2. Use a for of loop (not a typo - try it out! Try a for of loop) to call toUpperCase() on each of them and print out the result.
+const ninjaTurtles = ["Donatello", "Leonardo", "Raphael", "Michaelangelo"]
+for (let i = [0]; i <= 3; i++)
+   console.log(ninjaTurtles[i])
+   
 
 ////////////////////////////////
 // Methods, Revisited
